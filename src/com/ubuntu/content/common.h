2703317c64358a0c23337313bc741d74bc88171d
Ken VanDine <ken.vandine@canonical.com>
2013-08-29 14:20:52 -0400

    Use const QLatin1String instead of defines

--- src/com/ubuntu/content/common.h
+++ src/com/ubuntu/content/common.h
@@ -1,9 +1,29 @@
+/*
+ * Copyright © 2013 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Ken VanDine <ken.vandine@canonical.com>
+ */
+
 #ifndef COMMON_H
 #define COMMON_H
 
-#define HUB_SERVICE_NAME "com.ubuntu.content.dbus.Service"
-#define HUB_SERVICE_PATH "/"
-#define HANDLER_NAME_TEMPLATE "com.ubuntu.content.handler.%1"
-#define HANDLER_PATH "/com/ubuntu/content/transfer/ImportExportHandler"
+#include <QLatin1String>
+
+const QLatin1String HUB_SERVICE_NAME = QLatin1String("com.ubuntu.content.dbus.Service");
+const QLatin1String HUB_SERVICE_PATH = QLatin1String("/");
+const QLatin1String HANDLER_NAME_TEMPLATE = QLatin1String("com.ubuntu.content.handler.%1");
+const QLatin1String HANDLER_PATH = QLatin1String("/com/ubuntu/content/transfer/ImportExportHandler");
 
 #endif // COMMON_H

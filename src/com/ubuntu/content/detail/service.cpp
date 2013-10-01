e04ce385e7560abd0f8a36a35c568a8dff479fa8
Ken VanDine <ken.vandine@canonical.com>
2013-10-01 13:56:44 -0400

    invoke destination on charged

--- src/com/ubuntu/content/detail/service.cpp
+++ src/com/ubuntu/content/detail/service.cpp
@@ -253,8 +253,6 @@ QDBusObjectPath cucd::Service::CreateExportForTypeFromPeer(const QString& type_i
 
     Q_UNUSED(type_id);
 
-    d->app_manager->invoke_application(transfer->destination().toStdString());
-
     return QDBusObjectPath{source};
 }
 
@@ -266,6 +264,7 @@ void cucd::Service::handle_transfer(int state)
     if (state == cuc::Transfer::charged)
     {
         qDebug() << Q_FUNC_INFO << "Charged";
+        d->app_manager->invoke_application(transfer->destination().toStdString());
         this->connect_import_handler(transfer->destination(), transfer->import_path());
     }
 

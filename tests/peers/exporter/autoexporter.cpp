3a2ded0b40cb5a5f16eded3a62a7fabc479a22c9
Ken VanDine <ken.vandine@canonical.com>
2014-07-10 15:49:03 -0400

    Added new binary content-hub-testability which provides files and utilities for automated testing of content-hub

---
+++ tests/peers/exporter/autoexporter.cpp
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2013 Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Ken VanDine <ken.vandine@canonical.com>
+ */
+
+#include "autoexporter.h"
+
+AutoExporter::AutoExporter()
+{
+    auto hub = cuc::Hub::Client::instance();
+    hub->register_import_export_handler(this);
+}
+
+void AutoExporter::handle_import(cuc::Transfer *transfer)
+{
+    qDebug() << Q_FUNC_INFO << "not implemented";
+    Q_UNUSED(transfer);
+}
+
+void AutoExporter::handle_export(cuc::Transfer *transfer)
+{
+    qDebug() << Q_FUNC_INFO;
+    if (transfer == nullptr)
+    {
+        qDebug() << Q_FUNC_INFO << "Transfer null";
+        return;
+    }
+
+    QVector<cuc::Item> items;
+    items << cuc::Item(QUrl("file:///tmp/test1"));
+
+    if (transfer->selectionType() == cuc::Transfer::SelectionType::multiple) {
+        items << cuc::Item(QUrl("file:///tmp/test2"));
+    }
+
+    transfer->charge(items);
+
+    connect(transfer, SIGNAL(stateChanged()), this, SLOT(stateChanged()));
+
+    qDebug() << Q_FUNC_INFO << "Items:" << items.count();
+}
+
+void AutoExporter::handle_share(cuc::Transfer *transfer)
+{
+    qDebug() << Q_FUNC_INFO << "not implemented";
+    Q_UNUSED(transfer);
+}
+
+void AutoExporter::stateChanged()
+{
+    qDebug() << Q_FUNC_INFO;
+    cuc::Transfer *transfer = static_cast<cuc::Transfer*>(sender());
+
+    qDebug() << Q_FUNC_INFO << "STATE:" << transfer->state();
+
+
+    if (transfer->state() == cuc::Transfer::collected)
+        QCoreApplication::instance()->quit();
+}
+
+

984b58584aee015d540addef356e1e8cf13a1bf3
Ken VanDine <ken.vandine@canonical.com>
2016-12-05 12:25:30 -0500

    FIXME: Until bug #1647409 is fixed, we need to release any existing prompt sessions before starting a new one.

--- src/com/ubuntu/content/detail/service.cpp
+++ src/com/ubuntu/content/detail/service.cpp
@@ -527,12 +527,24 @@ void cucd::Service::setupPromptSession(QString app_id, uint clientPid)
     if (d->active_sessions.keys().contains(app_id))
         return;
 
+    /* FIXME: Until bug #1647409 is fixed, we need to release any existing 
+     * prompt sessions before starting a new one
+     */
+    Q_FOREACH(QString key, d->active_sessions.keys()) {
+        PromptSessionP pSession = d->active_sessions.value(key);
+        if (pSession.data()) {
+            qWarning() << "Removing session for" << key;
+            d->active_sessions.remove(key);
+            pSession->deleteLater();
+        }
+    }
+    /* End hack to work around bug #1647409 */
+
     PromptSessionP session = MirHelper::instance()->createPromptSession(clientPid);
     if (!session) return;
 
     QString mirSocket = session->requestSocket();
     TRACE() << Q_FUNC_INFO << "mirSocket:" << mirSocket;
-    qWarning() << Q_FUNC_INFO << "mirSocket:" << mirSocket;
 
     QObject::connect(session.data(), SIGNAL(finished()),
                      this, SLOT(onPromptFinished()));

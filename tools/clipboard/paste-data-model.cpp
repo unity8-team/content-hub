dcb9857be9312802e81a9b35e83fa378da32a131
Arthur Mello <arthur.mello@canonical.com>
2016-12-05 23:37:04 -0200

    Fix insertion order when updating model

--- tools/clipboard/paste-data-model.cpp
+++ tools/clipboard/paste-data-model.cpp
@@ -287,7 +287,7 @@ void PasteDataModel::updateModel()
         }
     }
 
-    for (int i = pasteData.size() - 1; i >= 0; i--) {
+    for (int i = 0; i < pasteData.size(); ; ++i) {
         addEntryByPasteId(pasteData.at(i));
     }
 
